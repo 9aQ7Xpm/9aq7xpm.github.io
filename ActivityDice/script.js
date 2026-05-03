fae0e0efe72e63544e1d1a0f990a79cec9b90fda
9aQ7Xpm <124165567+9aQ7Xpm@users.noreply.github.com>
2026-05-04 00:54:00 +0800

    Add files via upload

--- ActivityDice/script.js
+++ ActivityDice/script.js
@@ -2,6 +2,7 @@
     const Dice = $("#ActDice");
     const Lap = $("#ActDiceLap");
     const Trigger = $("#ActDiceTrigger");
+    const Reward = $(".ActDiceWrapper .RewardContainer")
 
     let Steps = 0;
     let Laps = 0;
@@ -148,6 +149,41 @@
         .then(() => IdleAnimation());
     };
 
+    // 规定稀有度
+    const Rarity = {
+        Green: "https://patchwiki.biligame.com/images/starengine/1/18/6rb63pcyo44967hdhz93p5ykjvuilwt.png",
+        Blue: "https://patchwiki.biligame.com/images/starengine/f/fe/0vfbwc0zty8ng946p593md62jestnnt.png",
+        Purple: "https://patchwiki.biligame.com/images/starengine/0/06/70yjl5wtjohggzfow1ymncwc685iibr.png",
+        Gold: "https://patchwiki.biligame.com/images/starengine/3/31/67ybp9rg6lhqjxxrgnt7dlu6v13vlko.png"
+    };
+    // 规定物品
+    const Goods = {
+        StarCoin: "https://patchwiki.biligame.com/images/starengine/5/58/282l0ethlcwxdocu9t3cavb15fm8wj6.png",
+        StarDisc: "https://patchwiki.biligame.com/images/starengine/3/31/765u24zeim8u5p6fzfsdriflkspyhp1.png",
+        Chip: "https://patchwiki.biligame.com/images/starengine/5/5a/nhw4m493c0ivoo7pt26aup97csnibnw.png",
+        GreenBook: "https://patchwiki.biligame.com/images/starengine/3/31/i76k7k8ybr7jveerc99iur74z1jxran.png",
+        BlueBook: "https://patchwiki.biligame.com/images/starengine/9/9a/q673aw2f00xvv49buszttbm700rjbgr.png",
+        Progress: "https://patchwiki.biligame.com/images/starengine/6/64/k8amvg8mlf7klxtn0xifywyn0hbbedf.png",
+    };
+
+    // 这是Platform_2的奖励列表
+    const PlatformTwo = [
+        [Rarity.Blue, Goods.StarCoin, 10],
+        [Rarity.Blue, Goods.StarCoin, 20],
+        [Rarity.Green, Goods.GreenBook, 1],
+        [Rarity.Green, Goods.GreenBook, 2],
+        [Rarity.Green, Goods.GreenBook, 3],
+        [Rarity.Purple, Goods.Progress, 20]
+    ];
+    // 这是Platform_3的奖励列表
+    const PlatformThree = [
+        [Rarity.Blue, Goods.StarCoin, 100],
+        [Rarity.Purple, Goods.StarDisc, 10],
+        [Rarity.Purple, Goods.StarDisc, 30],
+        [Rarity.Blue, Goods.BlueBook, 1],
+        [Rarity.Purple, Goods.Progress, 30]
+    ];
+
     // 点击按钮触发移动事件
     Trigger.on("click", function(){
         if(!CanTrigger) return;
@@ -163,11 +199,73 @@
                 CanTrigger = true;
             }, AnimateTime + 500);
 
-            // 这是再动
-            if([6, 12, 18].includes(Steps % 24)){
-                    setTimeout(() => {
+            // 这是Platform_2
+            if([1,4,5,8,10,11,13,15,17,19,20,22,23].includes(Steps % 24)){
+                setTimeout(() => {
+                    Reward.empty();
+                    let RandomNum = Math.floor(Math.random() * PlatformTwo.length);
+                    let Quailty = PlatformTwo[RandomNum][0];
+                    let Item = PlatformTwo[RandomNum][1];
+                    let Quantity = PlatformTwo[RandomNum][2];
+                    Reward.append(`
+                        <span style='font-size:40px;'>获得奖励</span>
+                        <div style='width:80px; height:80px; overflow:clip; position: relative;'>
+                            <img style='text-align:center;position: absolute;top: 0;left: 0;width:80px; height:80px;' src="${Quailty}">
+                            <img style='text-align:center;position: absolute;top: 0;left: 0;width:80px; height:80px;' src="${Item}">
+                            <img style='text-align:center;position: absolute;bottom: 0.25em;right: 0.25em;width:30px;' src="https://patchwiki.biligame.com/images/starengine/9/94/hy9u4mf71i30aty4lwdo0y81sp2wgoi.png">
+                            <div style='text-align:center;position: absolute;bottom: 0.15em;right: 0.7em;font-size: 10px;color: #fff;font-weight: bold;'>×${Quantity}</div>
+                        </div>`
+                    );
+                }, AnimateTime);
+            }/* 这是Platform_3 */ else if([2,7,14,21].includes(Steps % 24)){
+                setTimeout(() => {
+                    Reward.empty();
+                    let RandomNum = Math.floor(Math.random() * PlatformThree.length);
+                    let Quailty = PlatformThree[RandomNum][0];
+                    let Item = PlatformThree[RandomNum][1];
+                    let Quantity = PlatformThree[RandomNum][2];
+                    Reward.append(`
+                        <span style='font-size:40px;'>获得奖励</span>
+                        <div style='width:80px; height:80px; overflow:clip; position: relative;'>
+                            <img style='text-align:center;position: absolute;top: 0;left: 0;width:80px; height:80px;' src="${Quailty}">
+                            <img style='text-align:center;position: absolute;top: 0;left: 0;width:80px; height:80px;' src="${Item}">
+                            <img style='text-align:center;position: absolute;bottom: 0.25em;right: 0.25em;width:30px;' src="https://patchwiki.biligame.com/images/starengine/9/94/hy9u4mf71i30aty4lwdo0y81sp2wgoi.png">
+                            <div style='text-align:center;position: absolute;bottom: 0.15em;right: 0.7em;font-size: 10px;color: #fff;font-weight: bold;'>×${Quantity}</div>
+                        </div>`
+                    );
+                }, AnimateTime);
+            }/* 这是Platform_5 */ else if([3,9,16].includes(Steps % 24)){
+                setTimeout(() => {
+                    Reward.empty();
+                    Reward.append(`
+                        <span style='font-size:40px;'>获得奖励</span>
+                        <div style='width:80px; height:80px; overflow:clip; position: relative;'>
+                            <img style='text-align:center;position: absolute;top: 0;left: 0;width:80px; height:80px;' src="${Rarity.Purple}">
+                            <img style='text-align:center;position: absolute;top: 0;left: 0;width:80px; height:80px;' src="${Goods.Chip}">
+                            <img style='text-align:center;position: absolute;bottom: 0.25em;right: 0.25em;width:30px;' src="https://patchwiki.biligame.com/images/starengine/9/94/hy9u4mf71i30aty4lwdo0y81sp2wgoi.png">
+                            <div style='text-align:center;position: absolute;bottom: 0.15em;right: 0.7em;font-size: 10px;color: #fff;font-weight: bold;'>×10</div>
+                        </div>`
+                    );
+                }, AnimateTime);
+            }/* 这是再动 */ else if([6, 12, 18].includes(Steps % 24)){
+                setTimeout(() => {
+                    Reward.empty();
+                    Reward.append("<span style='font-size:40px;'>再行动一次！</span>");
                     Trigger.trigger('click');
                 }, AnimateTime +  500);
+            } else {
+                setTimeout(() => {
+                    Reward.empty();
+                    Reward.append(`
+                        <span style='font-size:40px;'>获得奖励</span>
+                        <div style='width:80px; height:80px; overflow:clip; position: relative;'>
+                            <img style='text-align:center;position: absolute;top: 0;left: 0;width:80px; height:80px;' src="${Rarity.Purple}">
+                            <img style='text-align:center;position: absolute;top: 0;left: 0;width:80px; height:80px;' src="${Goods.Progress}">
+                            <img style='text-align:center;position: absolute;bottom: 0.25em;right: 0.25em;width:30px;' src="https://patchwiki.biligame.com/images/starengine/9/94/hy9u4mf71i30aty4lwdo0y81sp2wgoi.png">
+                            <div style='text-align:center;position: absolute;bottom: 0.15em;right: 0.7em;font-size: 10px;color: #fff;font-weight: bold;'>×50</div>
+                        </div>`
+                    );
+                }, AnimateTime);
             };
 
             // 计算完成了多少圈
